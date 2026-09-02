605b603d5515a455594994e9a84afe138f6743cd
Bilal Ali <bilal.ali.irp.dev@gmail.com>
2026-09-02 21:53:30 +0300

    feat:fast cn replacment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,4 @@
-import { clsx } from "clsx";
-import type { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export { cn } from "cn";
 
 export function formatDate(dateString: string): string {
   const date = new Date(dateString);
